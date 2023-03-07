85090d7c93d8d60375919e45308c1e72f17d84fd
naitoyuma3230 <naitoyuma@outlook.jp>
2023-03-08 00:55:06 +0900

    jest

--- tests/unit/RegistrationDataTable.spec.ts
+++ tests/unit/RegistrationDataTable.spec.ts
@@ -1,24 +1,31 @@
 import { shallowMount } from '@vue/test-utils';
-import RegistrationComplete from '@/components/pages/RegistrationComplete.vue';
+import RegistrationDataTable from '@/components/parts/RegistrationDataTable.vue';
 
 describe('プロップステスト', () => {
-  it('keyを文字列として取得できるオブジェクトを渡す', () => {
-    interface DataObj {
-      [key: string]: string;
-    }
-
-    const companyData: DataObj = {
+  it('プロップスのオブジェクトを渡す', () => {
+    const companyData = {
       法人名: 'text',
       郵便番号: 'HELLO',
       住所: 'Hi',
     };
 
-    const wrapper = shallowMount(RegistrationComplete, {
+    const wrapper = shallowMount(RegistrationDataTable, {
       props: {
         registrationData: companyData,
       },
     });
 
-    expect(wrapper.find('th').exists()).toBeTruthy();
+    expect(wrapper.findAll('th').at(0)?.text()).toBe('法人名');
+    expect(wrapper.findAll('th').at(1)?.text()).toBe('郵便番号');
+    expect(wrapper.findAll('td').at(0)?.text()).toBe('text');
+    expect(wrapper.findAll('td').at(1)?.text()).toBe('HELLO');
+  });
+
+  it('プロップスを渡さない場合,table要素のみ描画される', () => {
+    const wrapper = shallowMount(RegistrationDataTable);
+
+    expect(wrapper.find('table').exists()).toBe(true);
+    expect(wrapper.find('th').exists()).toBe(false);
+    expect(wrapper.find('td').exists()).toBe(false);
   });
 });
